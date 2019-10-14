4718c3e9d399da93af4010443784f451bc7b2863
Ruifeng Wang <ruifeng.wang@arm.com>
2019-10-14 11:09:50 +0800

    check environment vm info.

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,6 +3,10 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
+uname -a
+ls -l /proc/sys
+ls -l /proc/sys/vm
+
 # setup hugepages
 cat /proc/meminfo
 sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'

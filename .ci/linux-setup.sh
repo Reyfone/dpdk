7656b7cd1aa578bd574464f728c2ae7c244fe9be
Ruifeng Wang <ruifeng.wang@arm.com>
2019-10-14 11:20:36 +0800

    require sudo.

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,10 +3,7 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-uname -a
-ls -l /proc/sys
-ls -l /proc/sys/vm
-
+id
 # setup hugepages
 cat /proc/meminfo
 sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'

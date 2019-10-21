40ee053450c60be5a480dcd4f673ad3c815d6892
Ruifeng Wang <ruifeng.wang@arm.com>
2019-10-21 15:58:56 +0800

    turn apparmor off to set hugepage

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -6,6 +6,11 @@ sudo python3 -m pip install --upgrade meson
 id
 ls -l /proc/sys/vm
 # setup hugepages
+# turn off apparmor to set hugepage from LXD
+if [[ "$TRAVIS_ARCH" == "arm64" ]]; then
+    sudo lxc config set vppjordan raw.lxc lxc.aa_profile=unconfined
+	sudo lxc restart vppjordan
+fi
 cat /proc/meminfo
 sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
 cat /proc/meminfo

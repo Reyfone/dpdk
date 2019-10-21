035285b1ca57d64138a9d3369768ce49c476e54f
Ruifeng Wang <ruifeng.wang@arm.com>
2019-10-21 16:43:04 +0800

    run lxc with sudo

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -6,9 +6,11 @@ sudo python3 -m pip install --upgrade meson
 # setup hugepages
 # turn off apparmor to set hugepage from LXD
 #if [[ "$TRAVIS_ARCH" == "arm64" ]]; then
+which lxc
 sudo apt-get install -y --no-install-suggests --no-install-recommends lxc
-lxc config set vppjordan raw.lxc lxc.aa_profile=unconfined
-lxc restart vppjordan
+which lxc
+sudo lxc config set vppjordan raw.lxc lxc.aa_profile=unconfined
+sudo lxc restart vppjordan
 #fi
 cat /proc/meminfo
 sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'

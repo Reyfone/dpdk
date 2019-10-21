17dcce8dfe05e764814d11e249bf6ac154f69b6d
Ruifeng Wang <ruifeng.wang@arm.com>
2019-10-21 16:09:32 +0800

    disable apparmor

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,14 +3,12 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-id
-ls -l /proc/sys/vm
 # setup hugepages
 # turn off apparmor to set hugepage from LXD
-if [[ "$TRAVIS_ARCH" == "arm64" ]]; then
+#if [[ "$TRAVIS_ARCH" == "arm64" ]]; then
     sudo lxc config set vppjordan raw.lxc lxc.aa_profile=unconfined
-	sudo lxc restart vppjordan
-fi
+    sudo lxc restart vppjordan
+#fi
 cat /proc/meminfo
 sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
 cat /proc/meminfo

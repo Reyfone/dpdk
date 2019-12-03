c58f8b433943f46acb3018f216909acf6f73c608
Ruifeng Wang <ruifeng.wang@arm.com>
2019-12-03 18:27:40 +0800

    remove debug ops, only add aarch64 compilation

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -5,11 +5,7 @@ sudo python3 -m pip install --upgrade meson
 
 # linux header and hugepage settings are skipped on arm64 due to environment limitation
 if [ "$TRAVIS_ARCH" != "aarch64" ]; then
-# add Canonical Kernel Team PPA for newer version kernel header package
-#    sudo add-apt-repository -y ppa:canonical-kernel-team/ppa
-#    sudo apt-get -q update
     sudo apt-get -y install linux-headers-$(uname -r)
-
 # setup hugepages
     cat /proc/meminfo
     sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'

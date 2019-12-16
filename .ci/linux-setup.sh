d675519a3f76e033a7a065e27a565407fe6cd8d5
Ruifeng Wang <ruifeng.wang@arm.com>
2019-12-16 17:26:47 +0800

    travis: use bionic on arm64 for linux header

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,9 +3,8 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-# linux header and hugepage settings are skipped on arm64 due to environment limitation
+# hugepage settings are skipped on arm64 due to environment limitation
 if [ "$TRAVIS_ARCH" != "aarch64" ]; then
-    sudo apt-get -y install linux-headers-$(uname -r)
 # setup hugepages
     cat /proc/meminfo
     sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'

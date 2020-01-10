3e3f997725377d774eb25649d4ff681a07cad115
Ruifeng Wang <ruifeng.wang@arm.com>
2020-01-10 11:23:32 +0800

    test for run_test instead of travis_arch when setting hugepage

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,9 +3,9 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-# hugepage settings are skipped on arm64 due to environment limitation
-if [ "$TRAVIS_ARCH" != "aarch64" ]; then
-# setup hugepages
+# skip hugepage settings if tests will not run
+if [ "$RUN_TESTS" = "1" ]; then
+    # setup hugepages
     cat /proc/meminfo
     sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
     cat /proc/meminfo

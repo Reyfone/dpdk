53df04672e85012293b3afa4d648a4a9fe4f8740
Ruifeng Wang <ruifeng.wang@arm.com>
2020-03-09 17:29:17 +0800

    add only applicable cases to test suite; ignore hugepage set result

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,16 +3,7 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade 'meson==0.47.1'
 
-# set up hugepage if fast-tests suite (default) will run
-if [ "$RUN_TESTS" = "1" ]; then
-    for testsuite in ${TEST_SUITES:-fast-tests}; do
-        if [ "$testsuite" = "fast-tests" ]; then
-            # setup hugepages
-            cat /proc/meminfo
-            sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
-            cat /proc/meminfo
-
-            break
-        fi
-    done
-fi
+# setup hugepages
+cat /proc/meminfo
+sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages' || true
+cat /proc/meminfo
